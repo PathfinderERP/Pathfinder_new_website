a89c3a7654e4441e16434cbe9929f7c890ba455e
Soumojit Saha <wbsoumo@Soumojits-MacBook-Air.local>
2026-09-19 23:38:56 +0530

    Fix missing Schedule and Chats component imports in AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -16,6 +16,7 @@ import MyCourses from "../pages/DigitalPart/MyCourses";
 import StudentResults from "../pages/DigitalPart/StudentResults";
 import Notices from "../pages/DigitalPart/Notices";
 import Schedule from "../pages/DigitalPart/Schedule";
+import Chats from "../pages/DigitalPart/Chats";
 import PhysicalAssets from "../pages/DigitalPart/PhysicalAssets";
 import ProtectedRoute from "../components/ProtectedRoute";
 import NotFound from "../pages/Notfound/NotFound";
